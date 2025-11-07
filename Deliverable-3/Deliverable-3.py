735fa1918c1f5f91c0d53a435cfef3bb9d39f422
musinicholas-debug <musinicholas@gmail.com>
2025-11-06 21:55:34 -0500

    added explanation for part 2 Q a

--- Deliverable-3/Deliverable-3.py
+++ Deliverable-3/Deliverable-3.py
@@ -11,12 +11,18 @@ df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
 
 #a)
 print(df["Electric Range"].value_counts().head(10))
+##using the head module combined with value_counts it gives us the top 10 Electric Range. 
 print(df.shape)
+#Showing that I have 250659 rows and 17 columns
 print(df.info())
+#Shows the number of non empty values for each column and gives the type of number/object it is.
 print(df.describe())
+#Gives you the  count, mean, standart deviation, min, 25%, 50%, 75% and max for column. 
 
 
 #b)
 print(df.duplicated())
 print(df.drop_duplicates())
+
+#c)
 
