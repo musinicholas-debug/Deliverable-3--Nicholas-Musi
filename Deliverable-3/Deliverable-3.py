ee5cba8bfc504da8201ac918384dfa0a47c61c42
musinicholas-debug <musinicholas@gmail.com>
2025-11-06 21:50:14 -0500

    did part 2 Q a and b

--- Deliverable-3/Deliverable-3.py
+++ Deliverable-3/Deliverable-3.py
@@ -1,8 +1,22 @@
+
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load the filtered CSV dataset
 df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
 
 
+#Preliminary steps
+
+#a)
+print(df["Electric Range"].value_counts().head(10))
+print(df.shape)
+print(df.info())
+print(df.describe())
+
+
+#b)
+print(df.duplicated())
+print(df.drop_duplicates())
 
