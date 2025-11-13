4bbbc4d309d928137e9c05143cfcf9dbb72317d8
musinicholas-debug <musinicholas@gmail.com>
2025-11-12 20:26:08 -0500

    Update Deliverable-3.py

--- Deliverable-3/Deliverable-3.py
+++ Deliverable-3/Deliverable-3.py
@@ -40,28 +40,26 @@ categorical_col = df.select_dtypes(exclude=["number"]).columns
 
 
 # Strategy:
-# Fill missing numerical values with the mean
-# Fill missing categorical values with 'Unknown'
-for col in numeric_col:
-    median_value = df[col].median()
-    df[col] = df[col].fillna(median_value)
+df[numeric_col] = df[numeric_col].fillna(df[numeric_col].median())
+df[categorical_col] = df[categorical_col].fillna("Unknown")
 
-for col in categorical_col:
-    df[col] = df[col].fillna("Unknown")
-    
 print(df.isnull().sum())
 #Explanation of code: for this part I had originaly coded it column by column creating a massive bunch of code, to simplify I used a loop. To start I split both my numerical and categorical columns into their own variables.
-#To then use them in the loop using the col function that I found and researched online. It let create a loop for the numerical and categorical values respectively allowing me to replace them with the median and "unknown".
+#To then use fillna to replace all numeric values missing with the median and all categorical values with unknow.
 
 
 
 #Keep all rows to preserve large dataset integrity. Use mean imputation for numeric columns to avoid data loss. Use “Unknown” for categorical columns to maintain clarity while keeping categorical values. 
 
-
 #d)
 #For my specific data set I have no collums that I need to convert.
 
+
+
 #3 Univariable non-graphical EDA
+
+
+
 
 
 #4 Univariable graphical EDA
@@ -73,6 +71,9 @@ print(df.isnull().sum())
 df_numerical = ["Electric Range", "Model Year","Base MSRP","Legislative District"]
 for i in df_numerical:
      sns.displot(data=df, x=i, hue="Electric Vehicle Type",kind="kde")
+
+
+
 
 
 #5 Multivariate non-graphical EDA
@@ -103,6 +104,48 @@ print(ctab3_p.head(10))
 three_way=pd.crosstab([df["County"],df["Electric Vehicle Type"]] ,df["Clean Alternative Fuel Vehicle (CAFV) Eligibility"], normalize="index")
 print(three_way.head(10))
 
+#6 Multivariate graphical EDA
+
+# For some plots, it's easier to work with a subset (e.g. top 5 makes)
+top_makes = df["Make"].value_counts().head(5).index
+df_top = df[df["Make"].isin(top_makes)].copy()
+#explanation of code: 
+
+
+#6.1.Visualizing statistical relationships (5 plots):
+
+#a) 
+sns.relplot(data=df_top , x="Model Year", y="Electric Range", col="Electric Vehicle Type")
+
+#b)
+sns.relplot( data=df_top, x="Model Year", y="Electric Range", hue="Electric Vehicle Type", size="Base MSRP", col="Make")
+
+#c)
+sns.lmplot( data=df, x="Model Year", y="Electric Range", hue="Electric Vehicle Type", scatter_kws={"alpha": 0.3})
+
+#6.2.Visualizing categorical data (10 plots):
+    
+#a) 
+sns.stripplot(data=df_top, x="Electric Vehicle Type", y="Electric Range", jitter=False)
+
+#b)
+sns.catplot(data=df_top, x="Electric Vehicle Type", y="Electric Range", hue="Make", kind="swarm")
+
+#c)
+
+#d)
+
+#e)
+
+#f)
+
+#g)
+
+#6.3. Visualizing bivariate distributions (3 plots):
+    
+#a)
+
+#b)
 
 
 
