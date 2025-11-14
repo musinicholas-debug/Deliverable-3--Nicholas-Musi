e3dc5f6e806959a6f54f4ae317e9e7f1337aebf3
musinicholas-debug <musinicholas@gmail.com>
2025-11-13 19:46:25 -0500

    Update test file.py

--- Deliverable-3/test file.py
+++ Deliverable-3/test file.py
@@ -1,26 +1,11 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load the filtered CSV dataset
 df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
-#c)
-print(df.isnull().sum())
-#Check missing values per column
 
-# Separate numeric and categorical columns
-numeric_col = df.select_dtypes(include=["number"]).columns
-categorical_col = df.select_dtypes(exclude=["number"]).columns
+top_makes = df["Make"].value_counts().head(5).index
+df_top = df[df["Make"].isin(top_makes)].copy()
 
 
-# Strategy:
-# Fill missing numerical values with the mean
-# Fill missing categorical values with 'Unknown'
-for col in numeric_col:
-    median_value = df[col].median()
-    df[col] = df[col].fillna(median_value)
-
-for col in categorical_col:
-    df[col] = df[col].fillna("Unknown")
-    
-print(df.isnull().sum())
